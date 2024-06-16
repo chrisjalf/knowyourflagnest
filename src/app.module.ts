4282648cdd5f9939a955946386bca1c02ab2eaef
Chris James <xcastroj97@gmail.com>
2024-06-16 22:25:17 +0800

    generate resource 'game-result'

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GameResultModule } from './game-result/game-result.module';
 
 @Module({
-  imports: [],
+  imports: [GameResultModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/game-result/game-result.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { GameResultService } from './game-result.service';
+
+@Controller('game-result')
+export class GameResultController {
+  constructor(private readonly gameResultService: GameResultService) {}
+
+  @Get('/test')
+  get() {
+    return { message: 'test' };
+  }
+}
