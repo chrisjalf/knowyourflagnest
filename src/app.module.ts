659b557cf964fdcd3de0de562ee5917bddb08947
Chris James <xcastroj97@gmail.com>
2024-06-18 21:06:04 +0800

    auth & users resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GameResultModule } from './game-result/game-result.module';
 import { KYFDataSourceModule } from './datasources/kyf.datasource';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [KYFDataSourceModule, GameResultModule],
+  imports: [KYFDataSourceModule, GameResultModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
